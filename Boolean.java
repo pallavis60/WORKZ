7ad729a7f8dcae75d9d51b0692fc3f790bcb4f24
pallavis60 <pallavi123.xworkz@gmail.com>
2023-03-02 02:16:56 +0530

    second code

---
+++ Boolean.java
@@ -0,0 +1,24 @@
+public class Boolean{
+	public static void main(String[]args){
+		boolean isClassStarted = true;
+		boolean isWorkCompleted = false;
+		boolean isWaterIsWorm = true;
+		boolean isTodayHoliday = false;
+		boolean isSundayIsHoliday = true;
+		boolean isThisLunchTime = false;
+		boolean isXworkzJavaInstitute = true;
+		boolean isBangaloreGardenCity = true;
+		boolean isStudentsAreComing = false;
+		boolean isDegreeExamsAreFinished = true;
+		System.out.println(isClassStarted);
+		System.out.println(isWorkCompleted);
+		System.out.println(isWaterIsWorm);
+		System.out.println(isTodayHoliday);
+		System.out.println(isSundayIsHoliday);
+		System.out.println(isThisLunchTime);
+		System.out.println(isXworkzJavaInstitute);
+		System.out.println(isBangaloreGardenCity);
+		System.out.println(isStudentsAreComing);
+		System.out.println(isDegreeExamsAreFinished);
+	}
+}
---
+++ Float.java
@@ -0,0 +1,24 @@
+public class Float{ 
+	public static void main(String[]args){
+		float percentageInDegree = 80.63F;
+		float valueOfPiInMathematics = 3.14F;
+		float avogadroNumberValue = 6.022F;
+		float discountGivenForCloths = 20.25F;
+		float intrestInBank = 20.5F;
+		float costOfChocolate = 30.50F; 
+		float volumeOfWater = 1.5F;
+		float temperatureOfHumanBodyInDegree = 98.6F;
+		float humidityLevel = 30.50F;
+		float permittivityInVacuume = 8.85F;
+		System.out.println(percentageInDegree);
+		System.out.println(valueOfPiInMathematics);
+		System.out.println(avogadroNumberValue);
+		System.out.println(discountGivenForCloths);
+		System.out.println(intrestInBank);
+		System.out.println(costOfChocolate);
+		System.out.println(volumeOfWater);
+		System.out.println(temperatureOfHumanBodyInDegree);
+		System.out.println(humidityLevel);
+		System.out.println(permittivityInVacuume);
+		}
+}
