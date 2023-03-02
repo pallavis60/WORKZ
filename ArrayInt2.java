83c9ce4517ea179043a7143a91a82801468a3e0d
pallavis60 <pallavi123.xworkz@gmail.com>
2023-03-02 22:10:32 +0530

    First code

---
+++ ArrayInt2.java
@@ -0,0 +1,8 @@
+public class ArrayInt2{
+	public static void main (String[]args){
+		int[]  marksOfStudent = {20, 30, 34, 67, 56};
+		int[] marks = new int[21];
+		System.out.println(marksOfStudent[3]);
+		
+	}
+}
---
+++ ArrayString2.java
@@ -0,0 +1,7 @@
+public class ArrayString2{
+	public static void main(String[]args){
+	String[] nameOfBirds = {"Owl", "Dove", "Penguin", "Parrot"};
+	String[] birds = new String[30];
+	System.out.println(nameOfBirds[3]);
+}
+}
