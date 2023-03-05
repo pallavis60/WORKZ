e6bb2a7564f1f49ca8fd6df161a9cf056908297b
pallavis60 <pallavi123.xworkz@gmail.com>
2023-03-05 21:23:02 +0530

    Third code

---
+++ Length.java
@@ -0,0 +1,8 @@
+public class Length{
+	public static void main(String[] args){
+		int[] marks ={20, 59, 78, 56, 78, 89, 90, 78, 56, 78, 78, 78, 89, 57,89};
+		
+			System.out.println(marks.length);
+	
+}
+}
