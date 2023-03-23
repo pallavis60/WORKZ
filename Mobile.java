dd90e5a4dd7d393067a081b5e9cff1e83dce7a1c
pallavis60 <pallavi123.xworkz@gmail.com>
2023-03-23 12:52:09 +0530

    23/3/23

---
+++ Mobile.java
@@ -0,0 +1,58 @@
+public class Mobile{
+	final String name = "Vivo";
+	final String model = "Y100";
+    final String simName = "Jio";
+	final String warrantyStatus = "OneYear";
+	final int warrantyCardNumber = 55678;
+	static long number = 9148960790L;
+	static String softwareVersion = "PD1928BF";
+	static String rom = "Function OS_9.1";
+	static long serviceCenterNumber = 9745363626L;
+	static byte lenses = 4;
+	int price = 29000;
+	byte storage = 64;
+	float screenSize = 5.5F;
+    String colour = "Black";
+	byte ram = 3;
+	String bluetoothAddress = "Unavailable";
+	int unlocknumber = 7676788;
+	String batteryStatus = "4352mAh";
+	String aboutCamera = "64MP";
+	boolean hasFingerPrintOption = true;
+	
+	
+	public void provideCommunication(){
+		
+	}
+	public void playsSongs(){
+		
+	}  
+	public void providesInformation(){
+		
+	}
+	public void storesData(){
+		
+	}
+	public void consumeCharge(){
+		
+	}
+	public void managesOurOfficialDocuments(){
+		
+	}
+	public void causeOfManyMentalAilments(){
+		
+	}
+	public void givesPrivacy(){
+		
+	}
+	public void helpsToTakePictures(){
+		
+	}
+	public void showTimeAndDates(){
+		
+	}
+	
+	
+	
+	
+}
